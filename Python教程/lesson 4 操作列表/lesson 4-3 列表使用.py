c470b3c31d2d8e9506914826e42c3f1bbef966d4
wangweixu <wangweixu@163.com>
2017-05-04 19:43:24 +0800

    Initial commit

--- Python教程/lesson 4 操作列表/lesson 4-3 列表使用.py
+++ Python教程/lesson 4 操作列表/lesson 4-3 列表使用.py
@@ -3,3 +3,14 @@
 print('----函数range()的使用----')
 for value in range(1,5):
     print(value)
+
+print('\n---切片，取前3个元素---')
+players = ['charles','martina','michael','florence','eli']
+print(players[0:3])
+
+print('\n---遍历切片---')
+players = ['charles','martina','michael','florence','eli']
+print("Here are the first three players on my team:")
+for players in players[:3]:
+    print(players.title())
+
