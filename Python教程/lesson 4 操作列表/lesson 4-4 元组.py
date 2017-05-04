f16bd5456d06450c81005c9e10e6862ebaf7d750
wangweixu <wangweixu@163.com>
2017-05-04 20:52:15 +0800

    新增

---
+++ Python教程/lesson 4 操作列表/lesson 4-4 元组.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+
+print('---元组定义---')
+dimensions = (200,50)
+print(dimensions[0])
+print(dimensions[1])
+
+print('\n---遍历元组中的所有值---')
+for dimension in dimensions:
+    print(dimension)
+
+print('\n---修改元组变量，将新的元组赋值给变量---')
+dimensions = (200,50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400,100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
