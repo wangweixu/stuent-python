00d9122a4ffe6a9a1f0a73f26e52c253e82e4ea5
wangweixu <wangweixu@163.com>
2017-05-04 20:19:27 +0800

    新增

--- Python教程/lesson 4 操作列表/lesson 4-3 列表使用.py
+++ Python教程/lesson 4 操作列表/lesson 4-3 列表使用.py
@@ -14,3 +14,22 @@ print("Here are the first three players on my team:")
 for players in players[:3]:
     print(players.title())
 
+print('\n---复制列表，将一个列表复制到另外一个列表---')
+my_foods = ['pizza','falafel','carrot cake']
+friend_foods = my_foods[:]
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print("My favorite foods are:")
+print(my_foods)
+print("\n My friend's favorite foods are:")
+print(friend_foods)
+
+print('\n---复制列表，指向同一个列表---')
+my_foods = ['pizza','falafel','carrot cake']
+friend_foods = my_foods
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print("My favorite foods are:")
+print(my_foods)
+print("\n My friend's favorite foods are:")
+print(friend_foods)
