28c4e22c4cf23ff51465f8ec8a9aad7473753261
wangweixu <wangweixu@163.com>
2017-05-24 22:19:07 +0800

    调整格式

--- Python教程/lesson 5 if语句/lesson 5-3 使用if处理列表.py
+++ Python教程/lesson 5 if语句/lesson 5-3 使用if处理列表.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 print("---检查特殊元素---")
-requested_toppings = ['mushrooms','green peppers','extra cheese']
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 for requested_topping in requested_toppings:
     if requested_topping == 'green peppers':
         print("Sorry, we are out of green peppers right now.")
@@ -18,8 +18,8 @@ else:
     print("Are you sure you want a plian pizza?")
 
 print("\n---使用多个列表---")
-available_toppings = ['mushroom','olives','green peppers','pepperoni','pineapple','extra cheese']
-requested_toppings2 = ['mushroom','french fries','extra cheese']
+available_toppings = ['mushroom', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings2 = ['mushroom', 'french fries', 'extra cheese']
 for requested_topping2 in requested_toppings2:
     if requested_topping2 in available_toppings:
         print("Adding " + requested_topping2 + ".")
