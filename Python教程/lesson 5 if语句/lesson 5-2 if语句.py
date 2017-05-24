3d16098e12d4037cd44365ebe9b5aeefcd6ec87e
wangweixu <wangweixu@163.com>
2017-05-24 22:21:04 +0800

    调整格式

--- Python教程/lesson 5 if语句/lesson 5-2 if语句.py
+++ Python教程/lesson 5 if语句/lesson 5-2 if语句.py
@@ -1,11 +1,11 @@
-#coding:utf-8
+# coding:utf-8
 
 print('---if语句---')
 age = 19
 if age >= 18:
     print("You are old enough to wotel")
 
-print('\n---"if-elif-else"语句---') #对单个代码块，若多个代码块，则要使用一系列单独的if语句。
+print('\n---"if-elif-else"语句---')  # 对单个代码块，若多个代码块，则要使用一系列单独的if语句。
 age = 19
 if age < 4:
     price = 0
