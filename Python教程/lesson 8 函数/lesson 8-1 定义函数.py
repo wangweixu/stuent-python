25a97a6d5ca3e415a5854df83c131769449419b1
wangweixu <wangweixu@163.com>
2017-08-27 22:32:34 +0800

    新增

---
+++ Python教程/lesson 8 函数/lesson 8-1 定义函数.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+print("---定义函数---")
+def greet_user():
+    """显示简单的问候语"""      # 文档字符串(docstring)的注释，描述函数是做什么的。
+    print("hello!")
+
+greet_user()
+
+print("\n---向函数传递信息---")
+def greet_user(uesrname):
+    """显示简单的问候语"""      # 文档字符串(docstring)的注释，描述函数是做什么的。
+    print("hello, " + uesrname.title() + "!")
+
+greet_user('jesse')
