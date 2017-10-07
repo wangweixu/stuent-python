40588e059e94954d4c94016accdc64334b772daa
wangweixu <wangweixu@163.com>
2017-10-07 16:54:48 +0800

    新增

--- Python教程/lesson 8 函数/lesson 8-3 返回值.py
+++ Python教程/lesson 8 函数/lesson 8-3 返回值.py
@@ -22,7 +22,7 @@ print("\n---返回字典---")
 
 def build_person(first_name, last_name, age=''):
     """返回一个字典，其中包含有关一个人的信息"""
-    person = {'first':first_name, 'last':last_name}
+    person = {'first': first_name, 'last': last_name}
     if age:
         person['age'] = age
     return person
