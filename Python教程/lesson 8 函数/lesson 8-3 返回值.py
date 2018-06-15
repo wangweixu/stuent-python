3740409c1d090501fa30df2bb73afcd332d02da0
wangweixu <wangweixu2008@163.com>
2018-06-15 21:14:21 +0800

    变更
    Signed-off-by: wangweixu <wangweixu2008@163.com>

--- Python教程/lesson 8 函数/lesson 8-3 返回值.py
+++ Python教程/lesson 8 函数/lesson 8-3 返回值.py
@@ -10,7 +10,6 @@ def get_formatted_name(first_name, last_name, middle_name=''):
         full_name = first_name + ' ' + last_name
     return full_name.title()    # 在函数中，可使用retrun语句将值返回到调用函数的代码行。
 
-
 musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
 musician = get_formatted_name('john', 'hooker', 'lee')
